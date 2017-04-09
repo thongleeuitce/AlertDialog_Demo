fc0f1a0f610370fd69ad5b8eb1363533c58e6fd5
Thong <thonglee.uit.ce@gmail.com>
2017-04-09 23:50:03 +0700

    completed custom alert dialog 1

--- app/src/main/java/com/example/thongle/lab03_1/CustomAlertDialog1Activity.java
+++ app/src/main/java/com/example/thongle/lab03_1/CustomAlertDialog1Activity.java
@@ -1,17 +1,59 @@
 package com.example.thongle.lab03_1;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.database.CursorIndexOutOfBoundsException;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.zip.Inflater;
 
 /**
  * Created by thongle on 08/04/2017.
  */
 
 public class CustomAlertDialog1Activity extends AppCompatActivity{
+    private Button button_dialog;
+    private TextView textView_name;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_alertdialog_1);
+
+        textView_name = (TextView) findViewById(R.id.txtv_custom_1);
+        button_dialog = (Button) findViewById(R.id.btn_custom_1);
+        button_dialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater layoutInflater = LayoutInflater.from(CustomAlertDialog1Activity.this);
+                View custom_dialog_1 = layoutInflater.inflate(R.layout.custom_dialog_1, null);
+
+
+                final AlertDialog custom_1 = new AlertDialog.Builder(CustomAlertDialog1Activity.this).create();
+                custom_1.setView(custom_dialog_1);
+                final EditText editText_name = (EditText) custom_dialog_1.findViewById(R.id.edtxt_enter_name);
+
+                custom_1.setButton(DialogInterface.BUTTON_POSITIVE, "OKE", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        textView_name.setText("Your name: " + editText_name.getText().toString());
+                    }
+                });
+                custom_1.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        custom_1.cancel();
+                    }
+                });
+                custom_1.show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/thongle/lab03_1/CustomAlertDialog2Activity.java
+++ app/src/main/java/com/example/thongle/lab03_1/CustomAlertDialog2Activity.java
@@ -2,6 +2,7 @@ package com.example.thongle.lab03_1;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -13,5 +14,6 @@ public class CustomAlertDialog2Activity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_alertdialog_2);
+
     }
 }
